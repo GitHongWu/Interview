ecba888049993af8697f8a14f570daaed2261c99
Hongcheng Wu <hongcheng1wu@gmail.com>
2021-03-11 01:20:16 -0600

    leetcode 121

--- LeetCode/121.买卖股票的最佳时机/Main.java
+++ LeetCode/121.买卖股票的最佳时机/Main.java
@@ -13,7 +13,8 @@ public class Main {
     public static void main(String[] args) {
         int[] array = new int[] {1, 4, 2};
         int[] array2 = new int[] {2, 4, 1};
-        buyStock(array2);
+        // buyStock(array2);
+        maxProfit(array);
     }
 
     // slow O(n^2), 每次i遍历i后面所有数
@@ -38,6 +39,23 @@ public class Main {
 
     //fast dp
     public static int maxProfit (int[] prices) {
-        
+        System.out.println(Arrays.toString(prices));
+
+        int n = prices.length;
+        if(n <= 1) return 0;
+
+        int dp[] = new int[n];
+        dp[0] = 0;
+        int max_profit = 0;
+        for(int i = 1; i < n; i++){
+            // 两种情况
+            // 1. 第i天买第i天卖，prices[i]-prices[i]=0
+            // 2. i天之前买第i天卖，dp[i-1] + prices[i] - prices[i-1]
+            dp[i] = Math.max(0, dp[i-1] + prices[i] - prices[i-1]);
+            max_profit = Math.max(max_profit, dp[i]);
+        }
+
+        System.out.println(Arrays.toString(dp));
+        return max_profit;
     }
 }
