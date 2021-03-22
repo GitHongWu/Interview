a39b63f150028154a33dea5595edd19c1ff1ebe1
Hongcheng Wu <hongcheng1wu@gmail.com>
2021-03-21 23:51:58 -0500

    3/21

--- Java/basic/DP/零钱问题/Main.java
+++ Java/basic/DP/零钱问题/Main.java
@@ -16,4 +16,28 @@ public class Main {
         // 不用第n种货币：dp[n-1][m]，为什么不使用第n种货币呢，因为penney[n]>m。
         // 这样就可以求出当m>=penney[n]时 dp[n][m] = dp[n-1][m]+dp[n-1][m-peney[n]]，否则，dp[n][m] = dp[n-1][m]
     }
+    
+    public int countWays(int[] penny, int n, int aim) {  
+        // write code here  
+        if(n==0||penny==null||aim<0){  
+         return 0;     
+        }  
+        int[][] pd = new int[n][aim+1];  
+        for(int i=0;i<n;i++){  
+         pd[i][0] = 1;     
+        }  
+        for(int i=1;penny[0]*i<=aim;i++){  
+         pd[0][penny[0]*i] = 1;     
+        }  
+        for(int i=1;i<n;i++){  
+            for(int j=0;j<=aim;j++){  
+                if(j>=penny[i]){  
+                    pd[i][j] = pd[i-1][j]+pd[i][j-penny[i]];  
+                }else{  
+                    pd[i][j] = pd[i-1][j];  
+                }  
+            }  
+        }  
+        return pd[n-1][aim];  
+    }
 }
