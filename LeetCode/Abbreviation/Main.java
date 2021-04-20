fe8b46c4fef586b2fccc6a1e308bc238e5419407
Hongcheng Wu <hongcheng1wu@gmail.com>
2021-04-19 23:52:08 -0500

    abbreviation leetcode

---
+++ LeetCode/Abbreviation/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+
+    // https://www.hackerrank.com/challenges/abbr/problem
+    // https://www.geeksforgeeks.org/check-possible-transform-one-string-another/
+    public static void main(String[] args) {
+    }
+}
