045f66e68fb85390c9b562cd678a0a5f9975553e
HongchengWu Lenove <hongcheng1wu@gmail.com>
2022-02-11 00:55:32 -0600

    leetcode 80

---
+++ C#/LeetCode/80. Remove Duplicates from Sorted Array II/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _80._Remove_Duplicates_from_Sorted_Array_II
+{
+    class Program
+    {
+        // https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
+        static void Main(string[] args)
+        {
+            int[] a = { 1, 1, 1, 2, 2, 3 };
+            System.Console.WriteLine( RemoveDuplicates(a) );
+            RemoveDuplicates(a);
+        }
+
+        static public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length <= 2) return nums.Length;
+
+            int slow = 2, fast = 2;
+            // System.Console.WriteLine(slow + " ");
+            for (int i = 2; i < nums.Length; i++)
+            {
+                if (nums[slow - 2] != nums[fast])
+                {
+                    nums[slow] = nums[fast];
+                    slow++;
+                }
+                fast++;
+            }
+            return slow;
+        }
+    }
+}
